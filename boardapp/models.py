93c661f531d75469026004e077b549879bbd4326
sakumayohei <yhi.sakuma@gmail.com>
2020-02-07 21:14:28 +0900

    second commit

---
+++ boardapp/models.py
@@ -0,0 +1,20 @@
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+
+
+class BoardModel(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    author = models.CharField(max_length=100)
+    images = models.ImageField(upload_to='')
+
+    good = models.IntegerField(null=True, blank=True, default=0)
+    readtext = models.CharField(max_length=100, null=True, blank=True, default='a')
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
